17ccdd90a692b6ddadd19eae21104de47d410a1e
charlesxu27 <charlesxu27@gmail.com>
2023-01-28 14:42:22 -0800

    add landing, login, and registration pages

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0 left-0 bg-white w-screen">
+      <h1 className="text-black text-center align-top">
+        Copyright 2023 Charles Xu
+        <br></br>
+      </h1>
+    </div>
+  );
+}
---
+++ pages/register.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import RegistrationForm from '../components/RegistrationForm'
+
+export default function Register() {
+  return (
+    <>
+    <Navbar />
+    <RegistrationForm />
+    <Footer />
+    </>
+  )
+}
