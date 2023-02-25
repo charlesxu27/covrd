852051970297e6be864f7ec1681c8d82935efe31
charlesxu27 <charlesxu27@gmail.com>
2023-02-25 10:43:06 -0800

    Split component forms

--- pages/trynow.tsx
+++ pages/trynow.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import RequestForm from "../components/RequestForm";
+import CandidateForm from "../components/CandidateForm";
 
 export default function TryNow() {
   return (
     <>
       <Navbar />
-      <RequestForm />
+      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
+      <h2>Let's Get Started</h2>
+      <CandidateForm />
+
+      </div>
       <Footer />
     </>
   );
